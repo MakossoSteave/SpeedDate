392b9f0d5631a57bf48b5b31e909891b35d5dbd8
Steave <makossosteave27@gmail.com>
2024-06-08 13:30:15 +0200

    add provideHTTPCLIENT

--- speed-dating/src/app/app.module.ts
+++ speed-dating/src/app/app.module.ts
@@ -7,13 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterModule } from './register/register.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule, RegisterModule ,  BrowserModule,
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, RegisterModule ,  BrowserModule,
      ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
